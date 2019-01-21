68ee85743c1bb449aeef992b2e905a85daa52a59
kjersbry <kjersbry@stud.ntnu.no>
2019-01-21 17:07:23 +0100

    Finished

---
+++ Part4/RESULT.md
@@ -0,0 +1,8 @@
+Implemented in C:
+
+Lots of zeroes, and some negative numbers. 
+
+Implemented in go/python:
+Numbers betweenn -1000000 and 1000000.
+
+
--- Part4/c/foo.c
+++ Part4/c/foo.c
@@ -6,19 +6,27 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+	for (int j=1; j<1000000;j++){
+		i =i+1;
+	}
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+	for (int j=1; j<1000000;j++){
+		i =i-1;
+	}
     return NULL;
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
-    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
-    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
+    
+	pthread_t incrementingThread, decrementingThread;
+	pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
+    	pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
